0c51c3d6f39cf52f01605cb63f1bc68ce631cc69
Georgi Parvanov <56466239+gparvanov@users.noreply.github.com>
2020-06-20 18:33:59 +0300

    updated task

---
+++ Defining Classes - Exercise/07. Raw Data/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] dataParts = Console.ReadLine().Split();
+                string model = dataParts[0];
+                int engineSpeed = int.Parse(dataParts[1]);
+                int enginePower = int.Parse(dataParts[2]);
+                int cargoWeight = int.Parse(dataParts[3]);
+                string cargoType = dataParts[4];
+                double tire1Pressure = double.Parse(dataParts[5]);
+                int tire1Age = int.Parse(dataParts[6]);
+                double tire2Pressure = double.Parse(dataParts[7]);
+                int tire2Age = int.Parse(dataParts[8]);
+                double tire3Pressure = double.Parse(dataParts[9]);
+                int tire3Age = int.Parse(dataParts[10]);
+                double tire4Pressure = double.Parse(dataParts[11]);
+                int tire4Age = int.Parse(dataParts[12]);
+                Tire[] tires = new Tire[4]
+                {
+                    new Tire(tire1Pressure,tire1Age),
+                    new Tire(tire2Pressure,tire2Age),
+                    new Tire(tire3Pressure,tire3Age),
+                    new Tire(tire4Pressure,tire4Age),
+                };
+                Engine engine = new Engine(engineSpeed, enginePower);
+                Cargo cargo = new Cargo(cargoWeight, cargoType);
+                Car currentCar = new Car(model,engine,cargo,tires);
+                cars.Add(currentCar);
+
+            }
+            string filter = Console.ReadLine();
+
+            foreach (var piece in cars)
+            {
+                if(filter == "flamable")
+                {
+                    if(piece.Cargo.CargoType == "flamable" && piece.Engine.EnginePower> 250) Console.WriteLine($"{piece.Model}");
+                }
+                else if (filter == "fragile")
+                {
+                    if (piece.Cargo.CargoType == "fragile")
+                    {
+                        foreach (var tire in piece.Tires)
+                        {
+                            if(tire.Pressure <1)
+                            {
+                                Console.WriteLine($"{piece.Model}");
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Defining Classes - Exercise/07. Raw Data/StartUp.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private Engine engine;
+
+        public Engine Engine
+        {
+            get { return engine; }
+            set { engine = value; }
+        }
+
+        private Tire[] tires;
+
+        public Tire[] Tires
+        {
+            get { return tires; }
+            set { tires = value; }
+        }
+
+        private Cargo cargo;
+
+        public Cargo Cargo
+        {
+            get { return cargo; }
+            set { cargo = value; }
+        }
+        public Car(string model, Engine engine, Cargo cargo, Tire[] tires)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Cargo = cargo;            
+            this.Tires = tires;
+        }
+    }
+
+    public class Engine
+    {
+        private int engineSpeed;
+        public int EngineSpeed
+        {
+            get { return engineSpeed; }
+            set { engineSpeed = value; }
+        }
+
+        private int enginePower;
+        public int EnginePower
+        {
+            get { return enginePower; }
+            set { enginePower = value; }
+        }
+
+        public Engine(int speed, int power)
+        {
+            this.EngineSpeed = speed;
+            this.EnginePower = power;
+        }
+    }
+
+    public class Tire
+    {
+        private double pressure;
+
+        public double Pressure
+        {
+            get { return pressure; }
+            set { pressure = value; }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public Tire(double pressure,int age)
+        {
+            this.Pressure = pressure;
+            this.Age = age;
+        }
+    }
+
+    public class Cargo
+    {
+        private string cargoType;
+
+        public string CargoType
+        {
+            get { return cargoType; }
+            set { cargoType = value; }
+        }
+        private int cargoWeight;
+
+        public int CargoWeight
+        {
+            get { return cargoWeight; }
+            set { cargoWeight = value; }
+        }
+
+        public Cargo(int weight, string type)
+        {
+            this.CargoWeight = weight;
+            this.CargoType = type;
+        }
+    }
+}
