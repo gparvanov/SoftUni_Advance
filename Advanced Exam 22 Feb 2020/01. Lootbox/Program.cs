917f2bfdf6034957dbbbded7b3050df05c10c586
Georgi Parvanov <56466239+gparvanov@users.noreply.github.com>
2020-06-23 00:35:13 +0300

    advance exams

---
+++ Advanced Exam 22 Feb 2020/01. Lootbox/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Lootbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> firstLoot = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Stack<int> secondLoot = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int sumClaimedItems = 0;
+            while(firstLoot.Any() && secondLoot.Any())
+            {
+                int firstLootElement = firstLoot.Peek();
+                int secondLootElement = secondLoot.Peek();
+
+                if((firstLootElement + secondLootElement) %2 == 0)
+                {
+                    sumClaimedItems += firstLootElement + secondLootElement;
+                    firstLoot.Dequeue();
+                    secondLoot.Pop();
+                }
+                else
+                {
+                    secondLoot.Pop();
+                    firstLoot.Enqueue(secondLootElement);
+                }
+                if (firstLoot.Count == 0 || secondLoot.Count == 0) break;
+            }
+            if (firstLoot.Count == 0) Console.WriteLine("First lootbox is empty");
+            else Console.WriteLine("Second lootbox is empty");
+            if (sumClaimedItems > 100) Console.WriteLine($"Your loot was epic! Value: {sumClaimedItems}");
+            else Console.WriteLine($"Your loot was poor... Value: {sumClaimedItems}");
+        }
+    }
+}
---
+++ Advanced Exam 22 Feb 2020/02. Re-Volt/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Re_Volt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
