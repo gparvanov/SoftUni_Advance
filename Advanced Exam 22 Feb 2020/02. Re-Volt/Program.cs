9f75ed53a5e62d92752ad7b2d3e75e1ab0cc1b93
Georgi Parvanov <56466239+gparvanov@users.noreply.github.com>
2020-06-23 20:54:45 +0300

    Exam feb 22 2020

--- Advanced Exam 22 Feb 2020/02. Re-Volt/Program.cs
+++ Advanced Exam 22 Feb 2020/02. Re-Volt/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace _02._Re_Volt
 {
@@ -7,7 +6,327 @@ namespace _02._Re_Volt
     {
         static void Main(string[] args)
         {
-            
-        }
+            int size = int.Parse(Console.ReadLine());
+            int commandsCount = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[size, size];
+            int startRow = 0;
+            int startCol = 0;
+            for (int row = 0; row < size; row++)
+            {
+                char[] colParts = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < size; col++)
+                {
+                    if(colParts[col] == 'f')
+                    {
+                        startRow = row;
+                        startCol = col;
+                    }
+                    matrix[row, col] = colParts[col];
+                }
+            }
+            int currentRow = startRow;
+            int currentCol = startCol;
+            bool won = false;
+            for (int i = 0; i < commandsCount; i++)
+            {
+                string direction = Console.ReadLine();
+                if (direction == "up")
+                {
+                    if (currentRow - 1 < 0)
+                    {
+                        matrix[currentRow, currentCol] = '-';
+                        currentRow = size -1;
+                        if(matrix[currentRow,currentCol] == 'F')
+                        {
+                            won = true;
+                            matrix[currentRow, currentCol] = 'f';
+                            break;
+                        }
+                        else if (matrix[currentRow, currentCol] == 'B')
+                        {
+                            if(currentRow -1 < 0)
+                            {
+                                currentRow = size - 1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                            else
+                            {
+                                currentRow -= 1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                        }
+                        else if (matrix[currentRow, currentCol] == 'T')
+                        {
+                            if (currentRow + 1 >= size)
+                            {
+                                currentRow = 0;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                            else
+                            {
+                                currentRow += 1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                        }
+                        else matrix[currentRow, currentCol] = 'f';
+                    }
+                    else
+                    {
+                        matrix[currentRow, currentCol] = '-';
+                        currentRow -= 1;
+                        if (matrix[currentRow, currentCol] == 'F')
+                        {
+                            won = true;
+                            matrix[currentRow, currentCol] = 'f';
+                            break;
+                        }
+                        else if (matrix[currentRow, currentCol] == 'B')
+                        {
+                            if (currentRow - 1 < 0)
+                            {
+                                currentRow = size -1 ;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                            else
+                            {
+                                currentRow -= 1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                        }
+                        else if (matrix[currentRow, currentCol] == 'T')
+                        {
+                            currentRow += 1;
+                            matrix[currentRow, currentCol] = 'f';
+                        }
+                        else matrix[currentRow, currentCol] = 'f';
+                    }
+                }
+                else if (direction == "down")
+                {
+                    if (currentRow + 1 >= size)
+                    {
+                        matrix[currentRow, currentCol] = '-';
+                        currentRow = 0;
+                        if (matrix[currentRow, currentCol] == 'F')
+                        {
+                            won = true;
+                            matrix[currentRow, currentCol] = 'f';
+                            break;
+                        }
+                        else if (matrix[currentRow, currentCol] == 'B')
+                        {
+                            if (currentRow + 1 >= size)
+                            {
+                                currentRow = 0;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                            else
+                            {
+                                currentRow += 1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                        }
+                        else if (matrix[currentRow, currentCol] == 'T')
+                        {
+                            if (currentRow - 1 < 0)
+                            {
+                                currentRow = size -1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                            else
+                            {
+                                currentRow -= 1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                        }
+                        else matrix[currentRow, currentCol] = 'f';
+                    }
+                    else
+                    {
+                        matrix[currentRow, currentCol] = '-';
+                        currentRow += 1;
+                        if (matrix[currentRow, currentCol] == 'F')
+                        {
+                            won = true;
+                            matrix[currentRow, currentCol] = 'f';
+                            break;
+                        }
+                        else if (matrix[currentRow, currentCol] == 'B')
+                        {
+                            if (currentRow + 1 >= size)
+                            {
+                                currentRow = 0;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                            else
+                            {
+                                currentRow += 1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                        }
+                        else if (matrix[currentRow, currentCol] == 'T')
+                        {
+                            currentRow -= 1;
+                            matrix[currentRow, currentCol] = 'f';
+                        }
+                        else matrix[currentRow, currentCol] = 'f';
+                    }
+                }
+                else if (direction == "left")
+                {
+                    if (currentCol -1 < 0)
+                    {
+                        matrix[currentRow, currentCol] = '-';
+                        currentCol = size -1 ;
+                        if (matrix[currentRow, currentCol] == 'F')
+                        {
+                            won = true;
+                            matrix[currentRow, currentCol] = 'f';
+                            break;
+                        }
+                        else if (matrix[currentRow, currentCol] == 'B')
+                        {
+                            if (currentCol - 1 < 0)
+                            {
+                                currentCol = size -1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                            else
+                            {
+                                currentCol -= 1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                        }
+                        else if (matrix[currentRow, currentCol] == 'T')
+                        {
+                            if (currentCol + 1 >= size)
+                            {
+                                currentCol = 0;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                            else
+                            {
+                                currentCol += 1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                        }                        
+                        else matrix[currentRow, currentCol] = 'f';
+                    }
+                    else
+                    {
+                        matrix[currentRow, currentCol] = '-';
+                        currentCol -= 1;
+                        if (matrix[currentRow, currentCol] == 'F')
+                        {
+                            won = true;
+                            matrix[currentRow, currentCol] = 'f';
+                            break;
+                        }
+                        else if (matrix[currentRow, currentCol] == 'B')
+                        {
+                            if (currentCol - 1 < 0)
+                            {
+                                currentCol = size -1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                            else
+                            {
+                                currentCol -= 1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                        }
+                        else if (matrix[currentRow, currentCol] == 'T')
+                        {
+                            currentCol += 1;
+                            matrix[currentRow, currentCol] = 'f';
+                        }
+                        else matrix[currentRow, currentCol] = 'f';
+                    }
+                }
+                else if (direction == "right")
+                {
+                    if (currentCol + 1 >= size)
+                    {
+                        matrix[currentRow, currentCol] = '-';
+                        currentCol = 0;
+                        if (matrix[currentRow, currentCol] == 'F')
+                        {
+                            won = true;
+                            matrix[currentRow, currentCol] = 'f';
+                            break;
+                        }
+                        else if (matrix[currentRow, currentCol] == 'B')
+                        {
+                            if (currentCol + 1 >= size)
+                            {
+                                currentCol = 0;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                            else
+                            {
+                                currentCol += 1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                        }
+                        else if (matrix[currentRow, currentCol] == 'T')
+                        {
+                            if (currentCol - 1 < 0)
+                            {
+                                currentCol = size -1 ;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                            else
+                            {
+                                currentCol -= 1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                        }
+                        else matrix[currentRow, currentCol] = 'f';
+                    }
+                    else
+                    {
+                        matrix[currentRow, currentCol] = '-';
+                        currentCol += 1;
+                        if (matrix[currentRow, currentCol] == 'F')
+                        {
+                            won = true;
+                            matrix[currentRow, currentCol] = 'f';
+                            break;
+                        }
+                        else if (matrix[currentRow, currentCol] == 'B')
+                        {
+                            if (currentCol + 1 >= size)
+                            {
+                                currentCol = 0;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                            else
+                            {
+                                currentCol += 1;
+                                matrix[currentRow, currentCol] = 'f';
+                            }
+                        }
+                        else if (matrix[currentRow, currentCol] == 'T')
+                        {
+                            currentCol -= 1;
+                            matrix[currentRow, currentCol] = 'f';
+                        }
+                        else matrix[currentRow, currentCol] = 'f';
+                    }
+                }                
+                if (won) break;
+            }
+            if (won) Console.WriteLine("Player won!");
+            else Console.WriteLine("Player lost!");
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    Console.Write(matrix[row,col]);
+                }
+                Console.WriteLine();
+            }            
+        }        
     }
 }
---
+++ Advanced Exam 22 Feb 2020/03. Guild/Guild.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string @class;
+
+        public string @Class
+        {
+            get { return @class; }
+            set { @class = value; }
+        }
+        private string rank;
+
+        public string Rank
+        {
+            get { return rank; }
+            set { rank = value; }
+        }
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public Player(string name, string @class)
+        {
+            this.Name = name;
+            this.@Class = @class;
+            this.Rank = "Trial";
+            this.Description = "n/a";
+        }
+        public override string ToString()
+        {
+            string toReturn =
+                @$"Player {this.Name}: {this.@Class}
+Rank: {this.Rank}
+Description: {this.Description}";
+            return toReturn;
+        }
+    }
+
+    public class Guild
+    {
+        private List<Player> roster;
+
+        public List<Player> Roster 
+        {
+            get { return roster; }
+            set { roster = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int capacity;
+
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+
+        public Guild(string name, int capacity) 
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            roster = new List<Player>();
+        }
+
+        public void AddPlayer(Player player)
+        {
+            if(roster.Count< Capacity)
+            {
+                roster.Add(player);
+            }
+        }
+        public int Count
+        {
+            get { return roster.Count; }            
+        }
+        public bool RemovePlayer( string name)
+        {            
+            for (int i = 0; i < roster.Count; i++)
+            {
+                if(roster[i].Name == name)
+                {
+                    roster.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void PromotePlayer(string name)
+        {
+            foreach (var player in roster)
+            {
+                if (player.Name == name && player.Rank != "Member") player.Rank = "Member";
+            }
+        }
+        public void DemotePlayer(string name)
+        {
+            foreach (var player in roster)
+            {
+                if (player.Name == name && player.Rank != "Trial") player.Rank = "Trial";                
+            }
+        }
+
+        public Player[] KickPlayersByClass(string @class)
+        {
+            List<Player> tempList = new List<Player>();
+            for (int i = 0; i < roster.Count; i++)
+            {
+                if (roster[i].@Class == @class)
+                {                    
+                    tempList.Add(roster[i]);
+                    roster.RemoveAt(i);
+                    if(roster.Count>0) i--;
+                }
+            }
+            return tempList.ToArray();
+        }
+
+        public string Report()
+        {
+            string toReturn = $@"Players in the guild: {this.Name}
+{string.Join(Environment.NewLine,roster)}";
+            return toReturn;
+        }
+    }
+}
