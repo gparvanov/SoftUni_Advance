c64289488e88e1b20a4b637014b08da5da5b26a8
Georgi Parvanov <56466239+gparvanov@users.noreply.github.com>
2020-07-13 16:48:15 +0300

    interface completed.

---
+++ Interfaces and Abstraction - Exercise/ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplicitInterfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+            }
+        }
+        private string country;
+        public string Country
+        {
+            get => country;
+            set
+            {
+                country = value;
+            }
+        }
+        private int age;
+        public int Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+            }
+        }
+
+        public Citizen(string name,string country,int age)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.Age = age;
+        }
+        string IPerson.GetName()
+        {
+            return $"{this.Name}";
+        }
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {this.Name}";
+        }        
+    }
+}
---
+++ Interfaces and Abstraction - Exercise/ExplicitInterfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputData = Console.ReadLine().Split();
+
+            while(inputData[0] != "End")
+            {
+                string name = inputData[0];
+                string country = inputData[1];
+                int age = int.Parse(inputData[2]);
+
+                IPerson citizen = new Citizen(name, country, age);
+                IResident citizen2 = new Citizen(name, country, age);
+                Console.WriteLine(citizen.GetName());
+                Console.WriteLine(citizen2.GetName());
+
+                inputData = Console.ReadLine().Split();
+            }
+        }
+    }
+}
